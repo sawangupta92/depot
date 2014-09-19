880ecc809b92f7985f1386dfce2be5826b596f4a
sawan <sawan.gupta92@gmail.com>
2014-09-19 14:19:57 +0530

    complete task E enhance cart (multiple items of same category are grouped)

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,11 +27,10 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
   product = Product.find(params[:product_id])
-  @line_item = @cart.line_items.build(product: product)
+  @line_item = @cart.add_product(product.id)
   respond_to do |format|
     if @line_item.save
-      format.html { redirect_to @line_item.cart,
-      notice: 'Line item was successfully created.' }
+      format.html { redirect_to @line_item.cart }
       format.json { render action: 'show',
       status: :created, location: @line_item }
     else
@@ -67,13 +66,14 @@ class LineItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def line_item_params
+    params.require(:line_item).permit(:product_id)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918134656) do
+ActiveRecord::Schema.define(version: 20140919063031) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -23,16 +23,17 @@ ActiveRecord::Schema.define(version: 20140918134656) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   default: 1
   end
 
-  add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
-  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
+  add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
+  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
 
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price"
+    t.decimal  "price",       precision: 10, scale: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
