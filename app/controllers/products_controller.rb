379cf7e72195b8263467901c99ffa2ed208909bd
sawan <sawan.gupta92@gmail.com>
2014-12-13 12:43:02 +0530

    try

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,5 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  caches_page :show
-  cache_sweeper :product_sweeper
   # GET /products
   # GET /products.json
   def index
--- app/sweepers/product_sweeper.rb
+++
@@ -1,9 +0,0 @@
-class ProductSweeper < ActionController::Caching::Sweeping::Sweeper
-  observe Product
-  def after_save(product)
-    expire_cache(product)
-  end
-  def expire_cache(product)
-    expire_page controller: :products, action: :show
-  end
-end
